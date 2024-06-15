3e8a082aaef3c7350c6040fe8e616a5dfacd3f0b
Kate Kozachenko <kottya1995@ukr.net>
2024-06-16 00:30:46 +0200

    5th hw done

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import aiohttp
 import asyncio
 import platform
 from datetime import timedelta, datetime
+from timing import async_timed
 
 
 class HttpError(Exception):
@@ -21,21 +22,23 @@ async def request(url: str):
         except (aiohttp.ClientConnectorError, aiohttp.InvalidURL) as err:
             raise HttpError(f'Connection error: {url}', str(err))
 
+@async_timed()
 async def main(days):
-    #curr_day = datetime.now() - timedelta(days=int(days))
-    curr_response = {}
+    curr_response = []
     curr_EUR = {}
+    curr_USD = {}
     for day in range(1,int(days)+1):
         curr_day = (datetime.now() - timedelta(days=int(day))).strftime("%d.%m.%Y")
         try:
             response = await request(f'https://api.privatbank.ua/p24api/exchange_rates?date={curr_day}')
-            print(f'{response}\n')
-            print(response['exchangeRate'])
-            for key, value in response['exchangeRate']:
-                curr_EUR[key] = value
-            print(curr_EUR)
-            curr_response[curr_day] = response
-            continue
+            for item in response['exchangeRate']:
+                if (item['baseCurrency'] == 'UAH') & (item['currency'] == 'EUR'):
+                    curr_EUR['sale'] = item['saleRateNB']
+                    curr_EUR['purchase'] = item['purchaseRateNB']
+                elif (item['baseCurrency'] == 'UAH') & (item['currency'] == 'USD'):
+                    curr_USD['sale'] = item['saleRateNB']
+                    curr_USD['purchase'] = item['purchaseRateNB']
+            curr_response.append({curr_day : {'EUR' : curr_EUR, 'USD' : curr_USD}})
         except HttpError as err:
             print(err)
     return curr_response
@@ -45,12 +48,8 @@ async def main(days):
 if __name__ == '__main__':
     if platform.system() == 'Windows':
         asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
-    print(sys.argv)
-    # if int(sys.argv[1]) >= 10:
-    #     raise HttpError("You only can request currency maximum for the last 10 days")
-    # else:
-    #     r = asyncio.run(main(sys.argv[1]))
-    #     print(r)
-
-    r = asyncio.run(main(2))
-    #print(r)
+    if int(sys.argv[1]) >= 10:
+        raise HttpError("You only can request currency maximum for the last 10 days")
+    else:
+        r = asyncio.run(main(sys.argv[1]))
+        print(json.dumps(r, indent=2, sort_keys=True))
